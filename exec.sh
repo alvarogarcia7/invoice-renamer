0abeea49dbe825fb493f6d2962205cdee90b429d
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2020-05-03 17:09:05 +0200

    Document main exec (after integration is done)

--- exec.sh
+++ exec.sh
@@ -5,9 +5,29 @@ set -euo pipefail
 function rename(){
   local file="$1"
   local renamer_pipe="commands.sh"
+
+  ## Clearing previous to start
   rm -f "${renamer_pipe}" && touch "${renamer_pipe}"
-  ./main.py --year=2020 --pipe="${renamer_pipe}" "${file}"
-  cat "${renamer_pipe}" >> commands_total.sh
+
+  ## Default Year?
+
+  # Default
+  #./main.py --year=2020 --pipe="${renamer_pipe}" "${file}"
+
+  # No Default
+  ./main.py --pipe="${renamer_pipe}" "${file}"
+
+
+  ## Command execution
+
+  # Instant
+  bash "${renamer_pipe}"
+
+  # Cumulate commands
+  #cat "${renamer_pipe}" >> commands_total.sh
+
+  ## Cleanup
+  rm -f "${renamer_pipe}"
 }
 
 rename "$1"
