393c335be0111fe37648e3b50edc92bebe49a084
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2020-05-03 13:30:49 +0200

    Refactor: create local variables for shell script

--- exec.sh
+++ exec.sh
@@ -2,6 +2,12 @@
 
 set -euo pipefail
 
-rm -f commands.sh && touch commands.sh
-./main.py --year=2020 --pipe=commands.sh "$1"
-cat commands.sh >> commands_total.sh
+function rename(){
+  local file="$1"
+  local renamer_pipe="commands.sh"
+  rm -f "${renamer_pipe}" && touch "${renamer_pipe}"
+  ./main.py --year=2020 --pipe="${renamer_pipe}" "${file}"
+  cat "${renamer_pipe}" >> commands_total.sh
+}
+
+rename "$1"
